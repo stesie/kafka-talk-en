881769626d71917cfe6ace18eddde2090441feb4
Stefan Siegl <stesie@brokenpipe.de>
2018-07-08 16:18:34 +0200

    add simple python-based producers

---
+++ examples/python_producer_with_keys.py
@@ -0,0 +1,17 @@
+from confluent_kafka import Producer
+
+
+p = Producer({'bootstrap.servers': 'localhost'})
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+for x in range(10):
+    p.produce('test.bar', 'das ist nur ein Test: %d' % x, 'Key: %d' % x, callback=delivery_report)
+
+p.flush()
